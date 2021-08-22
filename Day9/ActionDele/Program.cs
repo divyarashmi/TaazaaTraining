ac82a0c13345fd730eac40b329d47d79cce5322c
divyarashmi <divyarashmi>
2021-08-23 00:04:17 +0530

    my second commit

---
+++ Day9/ActionDele/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ActionDele
+{
+    class Program
+    {
+        public static void myfun(int p, int q)
+    {
+        Console.WriteLine(p - q);
+    }
+  
+    // Main method
+    static public void Main()
+    {
+  
+        // Using Action delegate
+        // Here, Action delegate 
+        // contains two input parameters
+        Action<int, int> val = myfun;
+        val(20, 5);
+    }
+    }
+}
