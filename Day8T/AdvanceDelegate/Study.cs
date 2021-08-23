95c83eea67faa976a09986cfc511fbbd7dbb6ed5
divyarashmi <divyarashmi>
2021-08-23 13:17:54 +0530

    delegate commit

---
+++ Day8T/AdvanceDelegate/Study.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Day8T.AdvanceDelegate
+{
+    public class Study
+    {
+        public string Name(string N)
+        {
+            return N;
+        }
+        public void PhoneNo()
+        {
+            Console.WriteLine(9852);
+        }
+        public bool StudyStatus()
+        {
+            return true;
+        }
+    }
+}
---
+++ Day8T/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Day8T.AdvanceDelegate;
+//delegate string NameDelegate(string N);
+delegate void PhoneNoDelegate();
+delegate bool StudyDelegate();
+
+namespace Day8T
+{
+    class Program
+    {
+        static void Main()
+        {
+            Study sobj = new Study();
+           // NameDelegate nameDelegate = new NameDelegate(sobj.Name);
+
+            //var n = nameDelegate.Invoke("Divya");
+            //Console.WriteLine(n);
+            Func<string,string> func = sobj.Name;
+            string fobj= func("Divya");
+            Console.WriteLine(fobj);
+        }
+    }
+}
