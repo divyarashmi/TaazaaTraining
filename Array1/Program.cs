db1e2b56a5aed03ee8dbf0df86e134182c5e23c1
divyarashmi <divyarashmi>
2021-08-24 11:10:27 +0530

    Array

---
+++ Array1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static int sum(int []arr, int n)
+    {
+         
+        int sum = 0; 
+         
+        
+        
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+         
+        return sum;
+    }
+     
+    // Driver method
+    public static void Main()
+    {
+         
+        int []arr = {12,3,4,15};
+        int n = arr.Length;
+         
+        Console.Write("Sum of given array is "+ sum(arr, n));
+    }
+ 
+    }
+}
